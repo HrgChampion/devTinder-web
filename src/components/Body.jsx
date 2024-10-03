ea8425ef9701fee28effe459516e9f44c28bc06f
Himanshu Gauba <himanshu.gauba@decisionpoint.in>
2024-10-04 00:12:23 +0530

    redirect to login if user is not authorized

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
+import { Outlet,useNavigate } from 'react-router-dom'
 import Footer from './Footer'
-
+import axios from 'axios'
+import { useDispatch,useSelector } from 'react-redux'
+import { BASEURL } from '../utils/constants'
+import {addUser} from '../utils/userSlice';
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) =>store.user);
+ const fetchUser = async() => {
+    try{
+  const res = await axios.get(BASEURL+ "/profile/view",{withCredentials: true});
+  dispatch(addUser(res.data));
+}catch(err){
+  navigate("/login")
+  console.error(err);
+}
+}
+
+useEffect(()=>{
+  if(!userData){
+  fetchUser()
+  }
+},[])
   return (
     <>
     <NavBar/>
--- src/utils/userSlice.js
+++ src/utils/userSlice.js
@@ -13,5 +13,5 @@ const userSlice = createSlice({
     }]
 });
 
-export const {addUse,removeUser} = userSlice.actions;
+export const {addUser,removeUser} = userSlice.actions;
 export default userSlice.reducer;
